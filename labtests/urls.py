93695c07141c820f67a36940abf6843789a288e4
SHREYASH <shreyash.dhage@gmail.com>
2026-02-25 17:08:11 +0530

    Update URLs and add API endpoints for tests and bookings

--- labtests/urls.py
+++ labtests/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import views
 
+# Touch file to trigger runserver autoreload when updated
+
 urlpatterns = [
     path('', views.home, name='home'),
     path('tests/', views.browse_tests, name='browse_tests'),
